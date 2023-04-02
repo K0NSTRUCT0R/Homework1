cc05a48461103ce9e2c5a193e02542bcd392164f
Andrey <andrey.kavetskiy@icloud.com>
2023-04-02 22:50:07 +0300

    Завершение работы по 1 задаче

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,55 @@
 ﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Hello, World!");
+Main ()
+
+void Main ()
+{
+    bool isWorking = true;
+
+    while (isWorking)
+    {
+        Console.WriteLine ("Input task");
+
+        string task = Console.ReadLine ();
+
+        switch (task)
+        {
+            case "exit": isWorking = false; break;
+            case "t1": Task1 (); break;
+            default:
+                break;
+        }
+    }
+}
+
+void Task1 ()
+{
+    int a = ReadInt ("a")
+    int b = ReadInt ("b")
+    int less = a;
+
+    if (!IsMoreThen (a, b))
+    {
+        less = b;
+    }
+    
+    Console.WriteLine (less);
+}
+
+int ReadInt (string argument)
+{
+    int number;
+
+    Console.Write($"Input {aegument}:");
+
+    while (!int.TryParse (Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number!");
+    }
+
+    return number;
+}
+
+bool IsMoreThen (int a, int b)
+{
+    return a > b;
+}
